c3e46e5849a3813aa20a56f2de95579e6c4244e8
Pablo <pablolucio_@hotmail.com>
2022-07-06 10:10:17 -0300

    Challenge finished#2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import swagger from 'swagger-ui-express'
+import swaggerJson from '../src/swagger.json'
+
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -6,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerJson))
 app.use("/users", usersRoutes);
 
 export { app };
